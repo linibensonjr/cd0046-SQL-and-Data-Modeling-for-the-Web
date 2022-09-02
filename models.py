e3c19bc2198ae6d8a058693e637de6d5a1f32ea4
linibensonjr <linibensonjr@gmail.com>
2022-09-02 14:59:54 +0100

    feat: Add update for Artist

---
+++ models.py
@@ -0,0 +1,43 @@
+from app import db
+
+
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+    def __repr__(self) -> str:
+        return f'<Venue: {self.id} {self.name}'
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+    def __repr__(self) -> str:
+        return f'<Artist: {self.id} {self.name}'
+
+class Show(db.Model):
+    __tablename__ = 'Show'
+    id = db.Column(db.Integer, primary_key=True)
+
+    def __repr__(self) -> str:
+        return f'<Show: {self.id} {self.name}'
+
+db.create_all()
